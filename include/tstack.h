8bc349642f6600115e750a7908eaf98c354e9761
Матвей Головенко <91070065+twysis@users.noreply.github.com>
2022-04-01 14:27:29 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,6 +4,31 @@
 
 template<typename T, int size>
 class TStack {
+  private:
+  T arr[100];
+  int top;
 
+ public:
+  TStack():top(-1) { }
+  T get() const {
+      return arr[top];
+  }
+  void pop() {
+  if (!isEmpty())
+    top--;
+  }
+  void push(T value) {
+  if (isFull())
+    throw "full";
+  else
+    arr[++top] = value;
+  }
+  bool isEmpty() const {
+    return top == -1;//пустота
+  }
+  bool isFull() const {
+    return top == size-1;//полнота
+  }
+};
 };
 #endif  // INCLUDE_TSTACK_H_
